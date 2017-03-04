f33e77b7b437a153c6f717690cb6dd2e90e1efc9
Simon Chauvin <contact@simonchauvin.com>
2017-03-04 09:43:52 +0100

    A slider, a plane and a material that gets modified by the slider's value.

---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private Material canvasMat;
+    private Slider colorSlider;
+
+
+	void Start ()
+    {
+        canvasMat = GameObject.Find("Klein").GetComponent<MeshRenderer>().material;
+        colorSlider = GameObject.Find("ColorSlider").GetComponent<Slider>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void setColor ()
+    {
+        HSVColor hsvColor = new HSVColor(colorSlider.value, 1f, 1f);
+
+        canvasMat.color = hsvColor.ToColor();
+    }
+}
