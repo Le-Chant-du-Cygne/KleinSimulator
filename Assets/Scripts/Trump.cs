0ea86d5fb784577c8036288d275c0b3b554e583e
AnnaGavaldaKedavra <rom1@hotmail.com>
2017-03-04 20:52:35 +0100

    TRUMP

---
+++ Assets/Scripts/Trump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trump : MonoBehaviour {
+
+    public GameObject ball;
+    private GameObject handle;
+
+	void Start () {
+
+        handle = GameObject.Find("Handle");
+
+    }
+	
+	void Update () {
+
+
+		
+	}
+
+   public void InstantiateBall()
+    {
+        GameObject myBall = Instantiate(ball, handle.transform.position, Quaternion.identity);
+        myBall.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-5, 6),2) * 120);
+
+    }
+
+
+}
