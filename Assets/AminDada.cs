d4975955054b5e2ec14690ef9c2bc2eeb25cccb5
AnnaGavaldaKedavra <rom1@hotmail.com>
2017-03-05 11:56:14 +0100

    AminDADA

---
+++ Assets/AminDada.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AminDada : MonoBehaviour {
+
+
+    public int mouseClicked;
+
+    void Start () {
+
+        mouseClicked = 0;
+
+    }
+	
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseClicked++;
+
+        }
+
+        if (mouseClicked > 500)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        if (mouseClicked > 501)
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+
+        }
+
+    }
+}
